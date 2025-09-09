e657788093007a85e61c898bb157e01566b360c3
sshahfatibks05-lang <sshahfatibks05@gmail.com>
2025-09-09 22:28:38 +0700

    Ini adalah Komentar

---
+++ src/Komentar.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, ini adalah tugas matakuliah Pemrograman Mobile")
+    //Contoh komentar satu baris
+    /*
+    Contoh komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
